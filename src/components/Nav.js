1e019fd7d090b59975e687a714add3af9f117002
Samuel-Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-04-22 14:34:17 +0100

    nav css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,34 +8,27 @@ export default function Navigation() {
   return (
     <nav className="navbar">
       <ul className="navbar outerList">
-        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          <div className="centerDiv">
+          {/* <div className="centerDiv"> */}
             <img className="logoImg" src={logoImg} alt="Logo"></img>
-          </div>
+          {/* </div> */}
           <Link className="logo" to="/Mint-Man-Records">
             Mint man
             <br />
             Records
           </Link>
-        </motion.div>
-        <ul className="navbar innerList">
+      
+        <ul className="innerList">
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
             <Link to="/Releases">Releases</Link>
           </motion.div>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <li>
               <Link to="/Artists">Artists</Link>
-            </li>
           </motion.div>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <li>
               <Link to="/Videos">Videos</Link>
-            </li>
           </motion.div>{" "}
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <li>
               <Link to="/info">Info</Link>
-            </li>
           </motion.div>
         </ul>
       </ul>
